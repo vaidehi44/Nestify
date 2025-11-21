7d109a1ac4f0bed77d9d3ec53c628a725d5ea237
vaidehi Atpadkar <vaide@Mac.elecom>
2025-11-22 00:49:50 +0900

    Initial commit

---
+++ src/main/java/com/nestify/NestifyApplication.java
@@ -0,0 +1,13 @@
+package com.nestify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestifyApplication.class, args);
+	}
+
+}
